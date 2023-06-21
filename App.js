114faa46affde5e9fdfa167f84f6e21fda0dfd92
Marcos Daniel <104729467+MarcosDanielMD@users.noreply.github.com>
2023-06-20 22:50:34 -0300

    SuperMercadin

---
+++ App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+import { Rotas } from './components/Rotas/rotas';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Rotas />
+    </NavigationContainer>
+     
+  );
+}
